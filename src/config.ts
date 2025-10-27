96f7fc84239b4c27a268065a565919d29eb8865f
Kevin Tun <kevin.tun@northwestern.edu>
2025-10-27 08:39:10 -0500

    add FeatureTrack with SPARQLAdapter

--- src/config.ts
+++ src/config.ts
@@ -41,9 +41,13 @@ const config = {
   ],
   tracks: [
     {
+      // Why is this property needed? What does it affect?
       type: "FeatureTrack",
+      // Other tracks or components might interact with this track and need an id to do so.
       trackId: "ncbi_genes",
+      // Determines the display name of this track in the track selector.
       name: "NCBI RefSeq Genes",
+      // Determines which assemblies the track applies to.
       assemblyNames: ["hg38"],
       adapter: {
         // An appropriate adapter type needs to be specified for the provided data to be parsed properly: Gff3TabixAdapter (gff.gz, gff.gz.tbi) or Gff3Adapter (gff.gz).
@@ -63,6 +67,22 @@ const config = {
         type: "VcfTabixAdapter",
         uri: "http://localhost:8080/ALL.wgs.shapeit2_integrated_snvindels_v2a.GRCh38.27022019.sites.vcf.gz",
       },
+    },
+    {
+      type: "FeatureTrack",
+      trackId: "sparql_test_features",
+      name: "SPARQL Test Features",
+      assemblyNames: ["hg38"],
+      category: ["SPARQL", "Testing"],
+      adapter: {
+        type: "SPARQLAdapter",
+        endpoint: {
+          uri: "http://localhost:8090/sparql",
+          locationType: "UriLocation"
+        },
+        queryTemplate: "SELECT ?uniqueId ?start ?end ?name ?note ?strand ?type WHERE { FILTER ( (?start >= {start}) && (?end <= {end}) ) }",
+        additionalQueryParams: []
+      }
     },
   ],
   // Allows text searching by gene names.
