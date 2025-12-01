1dc44626478974ccc2788bccacb79f478dced078
Kevin Tun <kevin.tun@northwestern.edu>
2025-12-01 08:34:02 -0600

    add test plugin to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react"
 import { createViewState, JBrowseApp } from "@jbrowse/react-app2"
+import { TestPlugin } from "./plugins/testPlugin"
 import './App.css'
 import config from "./config"
 
@@ -8,7 +9,7 @@ const App = () => {
   const [viewState, setViewState] = useState<ViewModel>()
 
   useEffect(() => {
-    const state = createViewState({ config })
+    const state = createViewState({ config, plugins: [TestPlugin] })
     setViewState(state)
   }, [])
 
